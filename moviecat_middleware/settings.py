77af757a817606ba091df44f22e7a2c0df706000
Dennis Ngeno <dennsngeno7@gmail.com>
2019-09-19 18:32:46 +0300

    Changed databse

--- moviecat_middleware/settings.py
+++ moviecat_middleware/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -80,6 +81,8 @@ DATABASES = {
     }
 }
 
+database_url = os.environ["DATABASE_URL"]
+DATABASES['default'] = dj_database_url.parse(database_url, conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
